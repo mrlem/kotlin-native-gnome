0818d36e129f049ff41b4860e9a85640dd3ac729
Sebastien Guillemin <sebastien.guillemin@gmail.com>
2021-04-19 21:47:38 +0200

    generator: remove obsolete dependency

--- gtk/generator/build.gradle.kts
+++ gtk/generator/build.gradle.kts
@@ -7,9 +7,6 @@ dependencies {
     // kotlin generation
     implementation("com.squareup:kotlinpoet:1.8.0")
 
-    // xml parsing - TODO - remove me when old parser is removed
-    implementation(localGroovy())
-
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
 
