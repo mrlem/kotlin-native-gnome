2c1aba072e26e340b9fd5bcbe45129c855960c45
Sebastien Guillemin <sebastien.guillemin@gmail.com>
2021-04-23 08:08:55 +0200

    binding: add maven publication section

--- gtk/binding/build.gradle.kts
+++ gtk/binding/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.net.URI
+
 plugins {
     kotlin("multiplatform")
     `maven-publish`
@@ -20,5 +22,47 @@ kotlin {
     val nativeMain by sourceSets.getting {
         languageSettings.useExperimentalAnnotation("kotlin.ExperimentalStdlibApi")
         languageSettings.useExperimentalAnnotation("kotlin.ExperimentalUnsignedTypes")
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "OSSRH"
+            url = URI.create("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = System.getenv("ORG_GRADLE_PROJECT_SONATYPE_NEXUS_USERNAME")
+                password = System.getenv("ORG_GRADLE_PROJECT_SONATYPE_NEXUS_PASSWORD")
+            }
+        }
+    }
+
+    publications.withType(MavenPublication::class.java).configureEach {
+        pom {
+            name.set(project.name)
+            description.set("GTK binding for Kotlin Native")
+            url.set("https://github.com/mrlem/kotlin-native-gnome")
+
+            licenses {
+                license {
+                    name.set("LGPL-2.1")
+                    url.set("https://github.com/mrlem/kotlin-native-gnome/blob/master/LICENSE")
+                }
+            }
+
+            developers {
+                developer {
+                    id.set("mrlem")
+                    name.set("Sébastien Guillemin")
+                    email.set("sebastien.guillemin@gmail.com")
+                }
+            }
+
+            scm {
+                connection.set("scm:git:ssh://github.com/mrlem/kotlin-native-gnome.git")
+                developerConnection.set("scm:git:ssh://github.com/mrlem/kotlin-native-gnome.git")
+                url.set("https://github.com/mrlem/kotlin-native-gnome")
+            }
+        }
     }
 }
