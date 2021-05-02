0602db9cb374982c37f9b0367fbe263cc2027c0c
Sebastien Guillemin <sebastien.guillemin@gmail.com>
2021-05-02 11:06:24 +0200

    bindings: simplify builder addition

--- bindings/gtk/src/nativeMain/kotlin/org/mrlem/gnome/gtk/Builder.kt
+++ bindings/gtk/src/nativeMain/kotlin/org/mrlem/gnome/gtk/Builder.kt
@@ -2,14 +2,14 @@
 
 package org.mrlem.gnome.gtk
 
-import interop.*
 import kotlin.String
 import org.gnome.gobject.Object
 import org.gnome.gtk.Builder
 import org.gnome.gtk.addFromString
+import org.gnome.gtk.getObject
 
 @Throws(Error::class)
 fun Builder.addFromString(text: String): UInt = addFromString(text, text.length.toULong())
 
-// TODO - why isn't this function mapped or todo?
-operator fun Builder.get(id: String): Object = gtk_builder_get_object(this, id)!!
+// TODO - maybe try to generalize to getXxx() methods?
+operator fun Builder.get(id: String): Object = getObject(id)!!
